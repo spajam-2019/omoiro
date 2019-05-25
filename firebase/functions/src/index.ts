6d0a238eb47ad114daef82164d6967c8414e8d2e
elipmoc <relipmoc101@gmail.com>
2019-05-26 06:58:37 +0900

    てきとうーにコントローラーとルーター作成

--- firebase/functions/src/index.ts
+++ firebase/functions/src/index.ts
@@ -1,12 +1,16 @@
 import * as functions from 'firebase-functions';
 
 import * as Express from "express";
+import indexRouter from "./routes/indexRouter";
+import omoiroRouter from "./routes/omoiroRouter";
+import userRouter from "./routes/userRouter";
 
 const app = Express();
 
-const indexRouter = require("./routes/indexRouter");
 app.use(Express.json());
 app.use(Express.urlencoded({ extended: false }));
 app.use('/', indexRouter);
+app.use('/omoiros', omoiroRouter);
+app.use('/users', userRouter);
 
 exports.app = functions.https.onRequest(app);
--- firebase/functions/src/routes/omoiroRouter.ts
+++ firebase/functions/src/routes/omoiroRouter.ts
@@ -2,7 +2,7 @@ import { Router } from "express";
 
 
 const router = Router();
-/* GET home page. */
+
 router.get('/', (req, res) => {
     res.json("Hello World");
 });
