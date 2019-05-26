78826a2a16ad4911f4c49e07e09904d32a6a61a1
hideyasu <kutuzov1228@gmail.com>
2019-05-26 11:07:18 +0900

    upload

--- android/app/src/main/java/httpClient/HttpClient.java
+++ android/app/src/main/java/httpClient/HttpClient.java
@@ -6,8 +6,7 @@ import android.util.Log;
 import androidx.annotation.NonNull;
 import androidx.arch.core.util.Function;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.android.gms.tasks.OnFailureListener;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.*;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
 import okhttp3.*;
@@ -17,25 +16,27 @@ import org.jetbrains.annotations.NonNls;
 import java.io.ByteArrayOutputStream;
 import java.net.URI;
 import java.util.function.Consumer;
+import java.util.UUID;
 
-public class HttpClient{
+public class HttpClient {
     OkHttpClient okHttpClient;
-    String baseUrl="https://us-central1-omoiro.cloudfunctions.net/app/";
-    ObjectMapper mapper=new ObjectMapper();
+    String baseUrl = "https://us-central1-omoiro.cloudfunctions.net/app/";
+    ObjectMapper mapper = new ObjectMapper();
 
-    public HttpClient(){
+    public HttpClient() {
         okHttpClient = new OkHttpClient.Builder().build();
     }
 
-    public void UploadFile(String fileName, byte[] bytes, Consumer<Uri> f){
+    public void UploadFile(String ext, byte[] bytes, Consumer<String> f) {
+
         FirebaseStorage storage = FirebaseStorage.getInstance("gs://omoiro.appspot.com/");
 
-
+        String fileName = UUID.randomUUID().toString() + "." + ext;
         // Create a storage reference from our app
         StorageReference storageRef = storage.getReference();
 
         // Create a reference to 'images/mountains.jpg'
-        StorageReference mountainsRef = storageRef.child("images/"+fileName);
+        StorageReference mountainsRef = storageRef.child("images/" + fileName);
 
         // Get the data from an ImageView as bytes
 
@@ -45,26 +46,31 @@ public class HttpClient{
             public void onFailure(@NonNull Exception exception) {
                 Log.e("UPLOAD ERROR", exception.toString());
             }
-        }).addOnSuccessListener(new SuccessListener(f));
+        }).continueWithTask(new Continuation<UploadTask.TaskSnapshot, Task<Uri>>() {
+            @Override
+            public Task<Uri> then(@NonNull Task<UploadTask.TaskSnapshot> task) throws Exception {
+                if (!task.isSuccessful()) {
+                    throw task.getException();
+                }
+
+                // Continue with the task to get the download URL
+                return mountainsRef.getDownloadUrl();
+            }
+        }).addOnCompleteListener(new OnCompleteListener<Uri>() {
+            @Override
+            public void onComplete(@NonNull Task<Uri> task) {
+                if (task.isSuccessful()) {
+                    Uri downloadUri = task.getResult();
+
+                    f.accept("https://firebasestorage.googleapis.com"+downloadUri.getPath());
+                }
+            }
+        });
     }
 
-    class SuccessListener implements OnSuccessListener<UploadTask.TaskSnapshot>{
-
-        Consumer<Uri> f;
-        public SuccessListener(Consumer<Uri> f){
-            this.f=f;
-        }
-
-        @Override
-        public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
-            f.accept(taskSnapshot.getStorage().getDownloadUrl().getResult());
-        }
-
-    }
-
-    public Omoiro[] GetOmoiros() throws Exception{
+    public Omoiro[] GetOmoiros() throws Exception {
         Request request = new Request.Builder()
-                .url(baseUrl+"omoiros")
+                .url(baseUrl + "omoiros")
                 .build();
 
         try (Response response = okHttpClient.newCall(request).execute()) {
@@ -77,16 +83,16 @@ public class HttpClient{
             if (response.body() != null) {
                 String body = response.body().string();
 
-                return mapper.readValue(body,Omoiro[].class);
+                return mapper.readValue(body, Omoiro[].class);
             }
         }
         throw new Exception("omoiros error");
     }
 
-    public  String CreateOmoiro(ReqOmoiro req) throws Exception{
+    public String CreateOmoiro(ReqOmoiro req) throws Exception {
         Request request = new Request.Builder()
-                .url(baseUrl+"omoiros/create")
-                .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"),mapper.writeValueAsString(req)))
+                .url(baseUrl + "omoiros/create")
+                .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"), mapper.writeValueAsString(req)))
                 .build();
 
         try (Response response = okHttpClient.newCall(request).execute()) {
@@ -99,16 +105,16 @@ public class HttpClient{
             if (response.body() != null) {
                 String body = response.body().string();
 
-                return mapper.readValue(body,ResId.class).id;
+                return mapper.readValue(body, ResId.class).id;
             }
         }
         throw new Exception("omoiros error");
     }
 
-    public  String AddEmoPush(ReqEmoPush req) throws Exception{
+    public String AddEmoPush(ReqEmoPush req) throws Exception {
         Request request = new Request.Builder()
-                .url(baseUrl+"emo_pushs/insert")
-                .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"),mapper.writeValueAsString(req)))
+                .url(baseUrl + "emo_pushs/insert")
+                .post(RequestBody.create(MediaType.parse("application/json; charset=utf-8"), mapper.writeValueAsString(req)))
                 .build();
 
         try (Response response = okHttpClient.newCall(request).execute()) {
@@ -121,7 +127,7 @@ public class HttpClient{
             if (response.body() != null) {
                 String body = response.body().string();
                 System.out.println(body);
-                return mapper.readValue(body,ResOk.class).ok;
+                return mapper.readValue(body, ResOk.class).ok;
             }
         }
         throw new Exception("emopush error");
